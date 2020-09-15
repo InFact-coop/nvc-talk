b28af331a1d0a88f90f8d6d8766824f2ced922f3
lucymk <lucy.m.king92@gmail.com>
2020-09-15 10:28:57 +0100

    fix things!

--- styles/components.js
+++ styles/components.js
@@ -1,5 +1,4 @@
-import React from "react";
-import styled, { createGlobalStyle } from "styled-components";
+import styled from "styled-components";
 import theme from "./theme";
 
 const { spacing, colors } = theme;
